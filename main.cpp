d0836a4fe06795818e44430104661c981e9df4fc
Arvi-H <arvihaxhillari1@gmail.com>
2022-09-21 18:50:05 -0600

    Created Project

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "Lexer.h"
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char** argv) {
+// File I/O
+    std::ifstream input;
+    std::stringstream in;
+
+// Convert file to a string stream
+    input.open(argv[1]);
+    in << input.rdbuf();
+    input.close();
+
+// Convert string stream to a string
+    std::string myFileString = in.str();
+
+// Run Lexer
+    Lexer* lexer = new Lexer();
+    lexer->Run(myFileString);
+
+    delete lexer;
+    return 0;
+}
